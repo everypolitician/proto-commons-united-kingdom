e76ff4b65b54b23a18a1bbe7d24f03918f2ea24b
GeoWill <will@mysociety.org>
2018-08-08 16:17:21 +0100

    Add scripts and queries for cities.

---
+++ boundaries/scripts/joiner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE=$1
+CSV=${BASE}.csv
+INSHP=${BASE}-ur.shp
+OUTSHP=${BASE}.shp
+
+
+ogr2ogr \
+	-sql "select '${BASE}-ur'.*, _OGR_1.* from '${BASE}-ur' left join '${CSV}'.'${BASE}' on '${BASE}-ur'.MS_FB = _OGR_1.MS_FB" \
+	-dialect sqlite \
+	${OUTSHP} \
+	${INSHP} \
+	-lco ENCODING=UTF-8
